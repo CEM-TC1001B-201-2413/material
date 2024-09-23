4d88575d858256e96bb747f234f278b9d6f8d96e
aram-gonzalez-tec <aram.gonzalez@tec.mx>
2024-09-23 10:55:03 -0600

    Introducción a Python

---
+++ 01 Introducción a Python/operaciones de casting.py
@@ -0,0 +1,5 @@
+# Operaciones de casting
+variable1 = float("150.34567")
+variable2 = 18
+resultado = variable1 + variable2
+print(resultado)
---
+++ 01 Introducción a Python/tipos de datos.py
@@ -0,0 +1,14 @@
+# Tipos de datos en Python
+print(type(150)) # int -> integer -> número entero
+print(type(35.2)) # float -> punto flotante -> número real
+print(type(10j)) # complex -> número complejo
+print(type(True)) # bool -> booleano -> Veradero
+print(type(False)) # Falso
+print(type("Hola")) # str -> string -> Texto
+print(type('Hola'))
+print(type([10,65.1,"hola",True,[1,2]]))
+# list -> Conjuntos ordenados y mutables de datos
+print(type((10,65.1,"hola",True,[1,2])))
+# tuple -> Conjunto ordenado e inmutable de datos
+print(type({"llave": "valor", 123: [1,2,3,5]}))
+# dict -> diccionario -> Conjunto ordenado, mutable y nombrado de datos
